e9085e9f1d440ec4ae0a927dcdd29eb90ea76ec9
Cauê Mendes <caue_1456mendes@outlook.com>
2024-04-13 18:43:29 -0300

    Minishell: Echo builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,30 +6,35 @@
 /*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 14:32:43 by csilva-m          #+#    #+#             */
-/*   Updated: 2024/04/13 18:16:09 by csilva-m         ###   ########.fr       */
+/*   Updated: 2024/04/13 18:42:19 by csilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int count_args(char **argv)
+static int	count_args(char **argv)
 {
-	int i = 0;
-	while(argv[i])
+	int	i;
+
+	i = 0;
+	while (argv[i])
 		i++;
-	return i;
+	return (i);
 }
 
-static int is_flag(char *str)
+static int	is_flag(char *str)
 {
-	int i = 0;
-	int flag = 0;
-	if(str[i] != '-')
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	if (str[i] != '-')
 		return (flag);
 	i++;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] == 'n')
+		if (str[i] == 'n')
 			flag = 1;
 		else
 			return (0);
@@ -40,24 +45,26 @@ static int is_flag(char *str)
 
 void	echo(char **argv)
 {
-	int argc = count_args(argv);
-	int flag = 0;
-	int i = 1;
+	int	argc;
+	int	flag;
+	int	i;
 
-	while(argv[i] && is_flag(argv[i]))
+	argc = count_args(argv);
+	flag = 0;
+	i = 1;
+	while (argv[i] && is_flag(argv[i]))
 	{
 		flag = 1;
 		i++;
 	}
-	while(i < argc)
+	while (i < argc)
 	{
 		printf("%s", argv[i]);
-		if(i < argc - 1)
+		if (i < argc - 1)
 			printf(" ");
 		i++;
 	}
-	if(!flag)
+	if (!flag)
 		printf("\n");
 	return (0);
 }
-
