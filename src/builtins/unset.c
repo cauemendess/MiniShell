07b909a277f7605f4ae44500831fce62c864cccd
Cauê Mendes <caue_1456mendes@outlook.com>
2024-04-17 19:01:35 -0300

    Minishell: New file

---
+++ src/builtins/unset.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 12:23:09 by csilva-m          #+#    #+#             */
+/*   Updated: 2024/04/17 12:55:53 by csilva-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+//void	delete_env(char *key)
+//{
+//	t_env	*env;
+//	t_env	*prev;
+//	env = get_core()->env_list;
+//	while(env != NULL && ft_strncmp(env->key, key, ft_strlen(key)) != 0)
+//	{
+//		prev = env;
+//		env = env->next;
+//	}
+//	if(env == NULL)
+//		return ;
+	
+	
+//}
+
+
+
+//void unset(char **argv)
+//{
+//	int i;
+//	i = 1;
+//	while(argv[i])
+//	{
+//		delete_env(argv[i]);
+//		i++;
+//	}
+//}
