aaa87eb7c5481190c1957d2a2a3c134aaae2f914
Cauê Mendes <caue_1456mendes@outlook.com>
2024-06-26 15:43:12 -0300

    Heredoc with quotes delimiter

--- src/operators/heredoc.c
+++ src/operators/heredoc.c
@@ -6,22 +6,37 @@
 /*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 15:42:56 by csilva-m          #+#    #+#             */
-/*   Updated: 2024/06/23 15:50:37 by csilva-m         ###   ########.fr       */
+/*   Updated: 2024/06/26 15:40:03 by csilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	child_process(char *limiter);
+void	child_process(char *limiter, t_bool flag);
 void	wait_child_heredoc(pid_t pid);
-void	prompt_heredoc(char *limiter);
+void	prompt_heredoc(char *limiter, t_bool flag);
 char	*expand_on_heredoc(char *line);
+
+
+t_bool	has_quote(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		if(str[i] == '"' || str[i] == '\'')
+			return(TRUE);
+		i++;
+	}
+	return(FALSE);
+}
 
 void	capture_heredoc(void)
 {
 	char	*limiter;
 	t_token	*cur;
+	t_bool	flag;
 
+	flag = FALSE;
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 	cur = get_core()->token;
@@ -29,17 +44,22 @@ void	capture_heredoc(void)
 	{
 		if (cur->token == HEREDOC && cur->next->token == WORD)
 		{
+			if(has_quote(cur->next->str))
+			{
+				flag = TRUE;
+				remove_quote(cur->next->str, cur);
+			}
 			get_core()->doc_fd = open("heredoc_tmp",
 					O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			limiter = cur->next->str;
 			if (get_core()->is_heredoc == FALSE)
-				child_process(limiter);
+				child_process(limiter, flag);
 		}
 		cur = cur->next;
 	}
 }
 
-void	child_process(char *limiter)
+void	child_process(char *limiter, t_bool flag)
 {
 	pid_t	pid;
 
@@ -47,7 +67,7 @@ void	child_process(char *limiter)
 	if (pid == 0)
 	{
 		signal(SIGINT, ctrl_c_heredoc);
-		prompt_heredoc(limiter);
+		prompt_heredoc(limiter, flag);
 	}
 	else
 		wait_child_heredoc(pid);
@@ -74,7 +94,7 @@ void	wait_child_heredoc(pid_t pid)
 	}
 }
 
-void	prompt_heredoc(char *limiter)
+void	prompt_heredoc(char *limiter, t_bool flag)
 {
 	char	*line;
 
@@ -86,7 +106,7 @@ void	prompt_heredoc(char *limiter)
 			free(line);
 			break ;
 		}
-		else if (ft_strcmp(line, "$"))
+		else if (flag == FALSE && ft_strcmp(line, "$"))
 			line = expand_on_heredoc(line);
 		ft_putendl_fd(line, get_core()->doc_fd);
 		free(line);
