052ebe51ec2881c33124fa45003db1616e64dbcb
Cauê Mendes <caue_1456mendes@outlook.com>
2024-06-26 15:35:33 -0300

    Close heredoc fd

--- src/signals/signals_heredoc.c
+++ src/signals/signals_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 00:44:45 by dfrade            #+#    #+#             */
-/*   Updated: 2024/06/23 15:05:56 by csilva-m         ###   ########.fr       */
+/*   Updated: 2024/06/26 14:17:50 by csilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	ctrl_c_heredoc(int signal)
 	ft_putstr_fd("\n", 1);
 	get_core()->is_heredoc = TRUE;
 	get_core()->exit_status = 130;
+	close(get_core()->doc_fd);
 	clear_child();
 }
